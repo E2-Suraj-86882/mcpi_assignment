399c68ca2d38668218a48b287e3b0e5b0c8fc9bc
Surajdahake0808 <surajdahake2017@gmail.com>
2024-10-07 18:01:47 +0530

    assign_10

---
+++ assign_10/assign10_q1/Inc/systick.h
@@ -0,0 +1,10 @@
+
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+#include"stm32f4xx.h"
+
+void systick_delay(uint32_t ms);
+
+#endif /* SYSTICK_H_ */
---
+++ assign_10/assign10_q1/Src/systick.c
@@ -0,0 +1,16 @@
+
+
+#include"systick.h"
+
+volatile uint32_t jiffies=0;
+
+void SysTick_Handler()
+{
+   jiffies++;
+}
+
+void systick_delay(uint32_t ms)
+{
+   uint32_t until = jiffies + ms;
+   while(jiffies < until);
+}
